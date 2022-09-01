8ab2061c0f47eeddd4e7045ec3036a80beec5810
yagoluis <yagoluisroberto@gmail.com>
2022-09-01 12:35:53 -0300

    Conectando MongoDB ao Node e Dotend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db('test');
+});
 
 
 
