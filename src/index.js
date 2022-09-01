a7f2c9d2619cc264c8d3f13460dbd9402cebe650
yagoluis <yagoluisroberto@gmail.com>
2022-09-01 12:17:06 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+app.listen(5000, () => {
+    console.log('listen on port 5000');
+})
