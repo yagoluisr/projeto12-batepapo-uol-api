0cb5f760f93ddbb0741e6ff4c8976f0155dbb117
yagoluis <yagoluisroberto@gmail.com>
2022-09-03 10:42:27 -0300

    post /participants: validação com joi e lb dayjs implementados

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
 dotenv.config();
 
@@ -16,18 +18,48 @@ mongoClient.connect().then(() => {
     db = mongoClient.db('uol');
 });
 
+const userSchema = joi.object({
+    name: joi.string().empty()
+});
+
+
+// console.log(dayjs());
+// console.log(dayjs().format( 'HH:mm:ss' ));
+// console.log(dayjs().format());
 
 app.post('/participants', async (req, res) => {
     const participant = req.body;
 
+    //console.log(participant.name);
+    //console.log({...participant, lastStatus: `${Date.now()}`});
 
-    if (!participant || typeof(participant.name) !== 'string' || participant.name === '') {
-        res.sendStatus(422);
+    const validationUser = userSchema.validate(participant, {abortEarly: false});
+
+    if (validationUser.error) {
+        const message = validationUser.error.details.map(obj => obj.message);
+        return res.status(422).send(message);
     }
 
     try {
+        const filterParticipant = await db.collection('participants').find({name: participant.name}).toArray();
+        console.log(filterParticipant);
+
+        if (filterParticipant.length !== 0){
+            return res.status(409).send('Já existe um usuário com esse nome');
+        }
+
         const user = await db.collection('participants').insertOne({...participant, lastStatus: `${Date.now()}`})    
         console.log(user);
+
+        const message = await db.collection('message').insertOne({
+            from: participant.name, 
+            to: 'Todos', 
+            text: 'entra na sala...', 
+            type: 'status', 
+            time: dayjs().format('HH:mm:ss')
+        });
+
+        console.log(message)
         res.sendStatus(201);
     } catch (error) {
         res.sendStatus(409);
